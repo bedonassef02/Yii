93d840e20f25181f1049056450c5ad08a8953b80
Bedo <bedonassef71@gmail.com>
2024-11-01 21:52:12 +0200

    Ad Hoc Validation

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,10 +3,27 @@
 namespace app\controllers;
 
 use app\models\RegistrationForm;
+use yii\base\DynamicModel;
 use yii\web\Controller;
 
 class SiteController extends Controller
 {
+
+    public function actionAdHocValidation() {
+        $model = DynamicModel::validateData([
+            'username' => 'John',
+            'email' => 'john@gmail.com'
+        ], [
+            [['username', 'email'], 'string', 'max' => 12],
+            ['email', 'email'],
+        ]);
+
+        if ($model->hasErrors()) {
+            var_dump($model->errors);
+        } else {
+            echo "success";
+        }
+    }
     public function actionRegistration() {
         $model = new RegistrationForm();
         return $this->render('registration', ['model' => $model]);
--- models/RegistrationForm.php
+++ models/RegistrationForm.php
@@ -1,36 +1,18 @@
 <?php
 namespace app\models;
+use app\components\CityValidator;
 use Yii;
 use yii\base\Model;
 class RegistrationForm extends Model {
     public $username;
     public $password;
     public $email;
-    public $subscriptions;
-    public $photos;
-    /**
-     * @return array customized attribute labels
-     */
-    public function attributeLabels() {
-        return [
-            'username' => 'Username',
-            'password' => 'Password',
-            'email' => 'Email',
-            'subscriptions' => 'Subscriptions',
-            'photos' => 'Photos',
-        ];
-    }
-
+    public $country;
+    public $city;
+    public $phone;
     public function rules() {
         return [
-            // the username, password, email, country, city, and phone attributes are required
-            [['password', 'email', 'country', 'city', 'phone'], 'required'],
-            ['username', 'required', 'message' => 'Username is required'],
-            ['country', 'trim'],
-            ['city', 'default'],
-            // the email attribute should be a valid email address
-            ['email', 'email'],
-            ['city', 'default', 'value' => 'Paris'],
+            ['city', CityValidator::className()]
         ];
     }
 }
